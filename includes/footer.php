ac1336fc164caf367b0f63fd7715ef40fcbfe687
Dennis Bögner <Dennis_boegner@web.de>
2025-11-21 15:22:44 +0100

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 <footer>
-    <p>MIT License - Erstellt von Dennis Bögner</p>
+    <p>MIT License - Erstellt von <a href="https://github.com/denni95112">Dennis Bögner</a></p>
+    <p>Version <?php echo APP_VERSION; ?></p>
+    <p>GitHub: <a href="https://github.com/denni95112/drohnen-flug-und-dienstbuch">https://github.com/denni95112/drohnen-flug-und-dienstbuch</a></p>
 </footer>
